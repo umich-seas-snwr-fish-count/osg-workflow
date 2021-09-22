220b0b8cc67de3855faf14cd0c0fa96916ec0ba7
Bennet Fauber <bennet@umich.edu>
2021-09-22 05:21:29 -0500

    Add script to remove problem files for Oct 2020

---
+++ clean_oct_2020.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+s3cmd rm "s3://testcou-seas-sonar-images/ARIS_2020_03_24/2020-03-24_220000_Frame_7445.png"
+s3cmd rm "s3://testcou-seas-sonar-images/ARIS_2020_03_24/2020-03-24_220000_Frame_7529.png"
+s3cmd rm "s3://testcou-seas-sonar-images/ARIS_2020_03_24/2020-03-24_220000_egm.png"
+s3cmd rm "s3://testcou-seas-sonar-images/ARIS_2020_09_30/2020-09-30_140000_Frame_0.png"
+s3cmd rm "s3://testcou-seas-sonar-images/ARIS_2020_09_30/2020-09-30_140000_Frame_44.png"
+s3cmd rm "s3://testcou-seas-sonar-images/ARIS_2020_10_02/2020-10-02_120900 (2).aris"
